665662e7df37a2a300b7079be2d9971e2260a6a9
yamada toshiya <yamd1048@gmail.com>
2017-04-10 14:42:58 +0900

    Lesson5 課題　得点表示 発展課題　タッチ入力
    タッチ入力の確認をUnity上で行う方法がわかりませんでした

---
+++ Assets/Score.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+
+    private GameObject scoreText;
+
+    private int score = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.scoreText = GameObject.Find("ScoreText");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.scoreText.GetComponent<Text>().text = "Score:" + score;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+
+        if (other.gameObject.tag == "SmallStarTag")
+        {
+            score += 10;
+        }
+        if (other.gameObject.tag == "LargeStarTag")
+        {
+            score += 30;
+        }
+        if (other.gameObject.tag == "SmallCloudTag")
+        {
+            score += 50;
+        }
+        if (other.gameObject.tag == "LargeCloudTag")
+        {
+            score += 100;
+        }
+
+    }
+
+}
